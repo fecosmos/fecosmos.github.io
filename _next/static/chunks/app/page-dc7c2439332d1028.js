1ef3a43aef61c0681aa61519cdd2bb90a07a626f
shibin-cli <shibiny6@gmail.com>
2025-04-06 05:05:11 +0000

    Deploying to gh-pages from @ fecosmos/fecosmos.github.io@b88a9bf0c675d2fab7f592299d953e5b75c8820a 🚀

---
+++ _next/static/chunks/app/page-dc7c2439332d1028.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{5812:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,563,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[563,80,77,358],()=>s(5812)),_N_E=e.O()}]);
